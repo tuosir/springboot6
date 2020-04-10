11dd2fcd72b853c4ea173e8267ae8ad7c4e3a218
tuosir <tuozhiyong123@qq.com>
2020-04-10 12:23:03 +0800

    添加springboot6项目

--- src/main/java/com/dayuzhou/springboot6/Springboot6Application.java
+++ src/main/java/com/dayuzhou/springboot6/Springboot6Application.java
@@ -2,8 +2,9 @@ package com.dayuzhou.springboot6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Springboot6Application {
 
     public static void main(String[] args) {
--- src/main/java/com/dayuzhou/springboot6/controller/HelloController.java
+++ src/main/java/com/dayuzhou/springboot6/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.dayuzhou.springboot6.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String helle(){
-        return "seccess";
+    @RequestMapping("/success")
+    public String hello(Map<String,Object> map){
+        map.put("hello","你好");
+        return "success";
     }
 }
