43dbe0429106efbb5076c26bf101695f5c3f6cb1
tuosir <tuozhiyong123@qq.com>
2020-04-13 22:36:01 +0800

    添加员工

--- src/main/java/com/dayuzhou/springboot6/controller/EmployeeController.java
+++ src/main/java/com/dayuzhou/springboot6/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.dayuzhou.springboot6.controller;
 
+import com.dayuzhou.springboot6.dao.DepartmentDao;
 import com.dayuzhou.springboot6.dao.EmployeeDao;
+import com.dayuzhou.springboot6.entities.Department;
 import com.dayuzhou.springboot6.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -13,6 +16,10 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
+
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
@@ -22,5 +29,20 @@ public class EmployeeController {
         //thymeleaf默认都会拼串
         //classpath:/templates/xxx.html
         return "emp/list";
+    }
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面，查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //员工添加
+    //springmvc自动将请求参数和入参对象的属性一一绑定
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //保存员工
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
