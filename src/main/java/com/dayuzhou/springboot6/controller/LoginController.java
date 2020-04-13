cd1a1faa5acccc0ed2c64e01e8d85285e0ea42ea
tuosir <tuozhiyong123@qq.com>
2020-04-13 21:27:36 +0800

    员工信息的添加

--- src/main/java/com/dayuzhou/springboot6/controller/LoginController.java
+++ src/main/java/com/dayuzhou/springboot6/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.dayuzhou.springboot6.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
