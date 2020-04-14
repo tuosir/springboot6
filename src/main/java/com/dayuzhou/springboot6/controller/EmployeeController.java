dd1ca0059a4502f27cb006d794482e6e65f6be9f
tuosir <tuozhiyong123@qq.com>
2020-04-14 23:46:00 +0800

    修改员工的controller

--- src/main/java/com/dayuzhou/springboot6/controller/EmployeeController.java
+++ src/main/java/com/dayuzhou/springboot6/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
@@ -43,6 +44,20 @@ public class EmployeeController {
     public String addEmp(Employee employee){
         //保存员工
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        //查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        //回到修改页面add是一个修改添加二合一的页面
+        return "emp/add";
+    }
+    public String updateEmployee(Employee employee){
+         System.out.println("修改员工信息"+employee);
         return "redirect:/emps";
     }
 }
