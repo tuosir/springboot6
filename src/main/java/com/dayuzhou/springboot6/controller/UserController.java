9c3cc6b3da457c08bc00004c1a1770d7748df3c4
tuosir <tuozhiyong123@qq.com>
2021-06-16 13:47:06 +0800

    git test

--- src/main/java/com/dayuzhou/springboot6/controller/UserController.java
+++ src/main/java/com/dayuzhou/springboot6/controller/UserController.java
@@ -16,8 +16,8 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable("id") Integer id){
-        User user = userRepository.getOne(id);
-        return user;
+        User usera = userRepository.getOne(id);
+        return usera;
     }
 
     @GetMapping("/user")
